ae727c436f1169d75f35c5759ba2067fd823d2c3
LunaKF <lunakhanjifustek@gmail.com>
2025-02-17 17:43:33 +0100

    shared

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SharedMenuUnroutedComponent } from "./shared/shared.menu.unrouted/shared.menu.unrouted/shared.menu.unrouted.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SharedMenuUnroutedComponent],
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SharedHomeRoutedComponent } from './shared/shared.home.routed/shared.home.routed/shared.home.routed.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: 'home', component: SharedHomeRoutedComponent },
+
+];
